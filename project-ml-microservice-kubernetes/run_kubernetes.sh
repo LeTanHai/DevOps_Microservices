2bfbbf0dc417e1a9d3feb279cc611665b15c83e8
Tan Hai <tanhaile98@gmail.com>
2022-05-31 23:59:57 +0700

    uodate code

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -8,13 +8,14 @@
 dockerpath=letanhai1998/devops-repo-project4:latest
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deploy devop-project4-app --image=$dockerpath
+# kubectl create deploy devop-project4-app --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pod/... --address 0.0.0.0 8080:80
+kubectl port-forward pod/devop-project4-app-54f5dd8f5c-kk89x --address 0.0.0.0 8080:80
 
 # Logs
+kubectl logs devop-project4-app-54f5dd8f5c-kk89x
